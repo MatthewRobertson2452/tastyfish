e37daca8d14985fcd167f785921b71f558ab38b6
Matthew Robertson <Matthew.Robertson@mi.mun.ca>
2021-02-05 13:14:40 -0330

    update TMB models

--- src/TMB/matrix_model_new.hpp
+++ src/TMB/matrix_model_new.hpp
@@ -18,6 +18,7 @@ Type matrix_model_new(objective_function<Type>* obj) {
   Type nll = 0.0;
   Type zero = 0.0;
   Type one = 1.0;
+  Type two = 2.0;
   
   //input data
   DATA_INTEGER(n);
@@ -26,90 +27,49 @@ Type matrix_model_new(objective_function<Type>* obj) {
   DATA_IVECTOR(iyear);
   DATA_IVECTOR(idex);
   DATA_SCALAR(k);
-  DATA_SCALAR(chi);
   DATA_VECTOR(pa);
-  DATA_IVECTOR(t2dex);
   
-  PARAMETER_MATRIX(yr_tau_mat);
   PARAMETER_VECTOR(iye);
-  PARAMETER(lbeta);
+  PARAMETER_VECTOR(lbeta);
+  PARAMETER_VECTOR(lchi);
   
-  
-  Type beta=exp(lbeta);
-  matrix<Type> resid(nyrs,ndex);
-  matrix<Type> mu(nyrs, ndex);
+  vector<Type> beta=exp(lbeta);
+  vector<Type> chi=exp(lchi);
+  vector<Type> mu(nyrs);
+  matrix<Type> new_mu(nyrs,ndex);
   
   //Observation Model
-  int id, iy, t2;
+  int id, iy;
   for(int i = 0;i < n;++i){
     iy = iyear(i);
     id = idex(i);
     
-    if(id==0){mu(iy,0) = exp(yr_tau_mat(iy,0))/(1+exp(yr_tau_mat(iy,0)));}
-    if(id==1){mu(iy,1) = exp(yr_tau_mat(iy,1))/(1+exp(yr_tau_mat(iy,1)));}
-    if(id==2){mu(iy,2) = exp(yr_tau_mat(iy,2))/(1+exp(yr_tau_mat(iy,2)));}
+    mu(iy) = exp(iye(iy))/(1+exp(iye(iy)));
+    
+    if(id==0){new_mu(iy,0)=mu(iy);}
+    if(id>0 & id<3){new_mu(iy,id)=(k*pow(mu(iy),beta(0)))/(pow(chi(0),beta(0))+pow(mu(iy),beta(0)));}
+    if(id>2 & id<5){new_mu(iy,id)=(k*pow(mu(iy),beta(1)))/(pow(chi(1),beta(1))+pow(mu(iy),beta(1)));}
+    //if(id>4 ){new_mu(iy,id)=(k*pow(mu(iy),beta(2)))/(pow(chi(2),beta(2))+pow(mu(iy),beta(2)));}
     
     if(isNA(pa(i))==false){
-      if(id==0){nll -= dbinom(pa(i), one, mu(iy,0), true);}
-      if(id==1){nll -= dbinom(pa(i), one, mu(iy,1), true);}
-      if(id==2){nll -= dbinom(pa(i), one, mu(iy,2), true);}
+      if(id==0){nll -= dbinom(pa(i), one, new_mu(iy,0), true);}
+      if(id>0){nll -= dbinom(pa(i), one, new_mu(iy,id), true);}
     }
     
   }
   
-  
-  matrix<Type> del_yr_tau=yr_tau_mat;
-  for(int j = 0;j < ndex;++j){
-    nll -= dnorm(del_yr_tau(0,j),Type(0.5),one, true);
-    for(int i = 1;i < nyrs;++i){
-      nll -= dnorm(del_yr_tau(i,j),del_yr_tau(i-1,j),one, true);
-    }}
-  
-  matrix<Type> new_mu(nyrs,ndex);
-  
-  
-  //Process Model //can examine yr_tau to eval difference in accuracy bwn Type 1 and Type 2 functional response assumption
-  for(int i = 0;i < n;++i){
-    iy = iyear(i);
-    id = idex(i);
-    t2 = t2dex(i);
-    
-    new_mu(iy,0)=mu(iy,0);
-    if(id==1){new_mu(iy,1)=pow((-((mu(iy,1)-k)*pow(chi,-beta))/mu(iy,1)),-1/beta);}
-    if(id==2){new_mu(iy,2)=pow((-((mu(iy,2)-k)*pow(chi,-beta))/mu(iy,2)),-1/beta);}
-    
-    if(t2 == zero){
-      
-      resid(iy,id) =  new_mu(iy,0) - iye(iy);} //for the trawl samples we assume they are proportional to pop size, so can just subtract values directly to get year effect
-    
-    if(t2 == one){
-      
-      resid(iy,id) = new_mu(iy,id) - iye(iy);}
-    
-  }
-  
-  
-  
   //RW on these likelihoods results in better estimates
   vector<Type> del_iye=iye;
-  nll -= dnorm(del_iye(0),Type(0.5),one, true);
+  nll -= dnorm(del_iye(0),Type(10.0),one, true);
   for(int i = 1;i < nyrs;++i){
     nll -= dnorm(del_iye(i),del_iye(i-1),one, true);
   }
   
-  for(int i = 0;i < n;++i){
-    iy = iyear(i);
-    id = idex(i);
-    t2 = t2dex(i);
-    nll -= dnorm(resid(iy,id), zero, one, true);
-  }
   
-  
-  REPORT(resid);
-  REPORT(yr_tau_mat);
   REPORT(mu);
   REPORT(iye);
   REPORT(beta);
+  REPORT(chi);
   REPORT(new_mu);
   
   ADREPORT(iye);
--- src/TMB/type1_model.hpp
+++ src/TMB/type1_model.hpp
@@ -8,71 +8,57 @@
 template <class Type>
   Type type1_model(objective_function<Type>* obj) {
 
-  using namespace density;
-  Type nll = 0.0;
-  Type zero = 0.0;
-  Type one = 1.0;
-  
-  //input data
-  DATA_INTEGER(n);
-  DATA_INTEGER(nyrs);
-  DATA_INTEGER(ndex);
-  DATA_IVECTOR(iyear);
-  DATA_IVECTOR(idex);
-  DATA_VECTOR(pa);
-  
-  PARAMETER_MATRIX(yr_tau);
-  PARAMETER_VECTOR(iye);
-  PARAMETER(log_sd_resid);
-  
-  matrix<Type> resid(nyrs,ndex);
-  Type sd_resid = exp(log_sd_resid);
-  matrix<Type> new_mu(nyrs,ndex);
-  matrix<Type> mu(nyrs,ndex);
-  
-  
-  
-  //Observation Model
-  int id, iy;
-  for(int i = 0;i < n;++i){
-    iy = iyear(i);
-    id = idex(i);
+    using namespace density;
+    Type nll = 0.0;
+    Type zero = 0.0;
+    Type one = 1.0;
+    Type two = 2.0;
     
-    mu(iy,id) = exp(yr_tau(iy, id))/(1+exp(yr_tau(iy, id)));
+    //input data
+    DATA_INTEGER(n);
+    DATA_INTEGER(nyrs);
+    DATA_INTEGER(ndex);
+    DATA_IVECTOR(iyear);
+    DATA_IVECTOR(idex);
+    DATA_VECTOR(pa);
     
-    if(isNA(pa(i))==false){
-      nll -= dbinom(pa(i), one, mu(iy,id), true); 
+    PARAMETER_VECTOR(iye);
+    PARAMETER(lk);
+    
+    vector<Type> mu(nyrs);
+    matrix<Type> new_mu(nyrs,ndex);
+    
+    //Observation Model
+    int id, iy;
+    for(int i = 0;i < n;++i){
+      iy = iyear(i);
+      id = idex(i);
+      
+      mu(iy) = exp(iye(iy))/(1+exp(iye(iy)));
+      
+      if(id==0){new_mu(iy,0)=mu(iy);}
+      if(id>0){new_mu(iy,id)=mu(iy);}
+      
+      if(isNA(pa(i))==false){
+        if(id==0){nll -= dbinom(pa(i), one, new_mu(iy,0), true);}
+        if(id>0){nll -= dbinom(pa(i), one, new_mu(iy,id), true);}
+      }
+      
     }
-  }
-  
-  
-  //Process Model //can examine yr_tau to eval difference in accuracy bwn Type 1 and Type 2 functional response assumption
-  for(int i = 0;i < n;++i){
-    iy = iyear(i);
-    id = idex(i);
     
-    resid(iy,id) =  mu(iy,id) - iye(iy);} //for the trawl samples we assume they are proportional to pop size, so can just subtract values directly to get year effect
-  
-  //RW on these likelihoods results in better estimates
-  vector<Type> del_iye=iye;
-  nll -= dnorm(del_iye(0),Type(0.5),one, true);
-  for(int i = 1;i < nyrs;++i){
-    nll -= dnorm(del_iye(i),del_iye(i-1),one, true);
-  }
-  
-  for(int j = 0;j < ndex;++j){
-    for(int i = 0;i < nyrs;++i){
-      nll -= dnorm(resid(i,j), zero, sd_resid, true); 
+    //RW on these likelihoods results in better estimates
+    vector<Type> del_iye=iye;
+    nll -= dnorm(del_iye(0),Type(10.0),one, true);
+    for(int i = 1;i < nyrs;++i){
+      nll -= dnorm(del_iye(i),del_iye(i-1),one, true);
     }
-  }
-  
-  
-  REPORT(resid);
-  REPORT(yr_tau);
-  REPORT(mu);
-  REPORT(iye);
-  
-  ADREPORT(iye);
+    
+    
+    REPORT(mu);
+    REPORT(iye);
+    REPORT(new_mu);
+    
+    ADREPORT(iye);
   
   return nll;
   }
